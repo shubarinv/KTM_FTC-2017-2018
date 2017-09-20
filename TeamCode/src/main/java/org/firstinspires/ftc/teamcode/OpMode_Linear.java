7250c8f3ab30eb9ce3cb4f70c76173577855ac73
Unknown <vhundef@gmail.com>
2017-09-20 22:26:01 +0300

    Changed control scheme

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Linear.java
@@ -91,7 +91,7 @@ public class OpMode_Linear extends LinearOpMode {
 
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
+            double drive = -gamepad1.right_stick_y;
             double turn  =  gamepad1.right_stick_x;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
