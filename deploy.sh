c280cc7bb130360e47d90dbe761ef94af52bd109
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 00:27:16 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,16 @@ sudo sshpass -p 'Vhn323884489' TeamCode/build/outputs/apk/TeamCode-debug.apk vhu
 quit
 END_SCRIPT
 exit 0
+#!/bin/bash
+HOST='files.000webhost.com'
+USER='vhundef'
+PASSWD='Vhn323884489'
+
+ftp $HOST <<END_SCRIPT
+user $USER $PASSWD
+cd /public_html/lastest
+put TeamCode/build/outputs/apk/TeamCode-debug.apk
+quit
+END_SCRIPT
+
+exit 0
