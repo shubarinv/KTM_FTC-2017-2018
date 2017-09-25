7faf42f1caf4bdfe60b81c27108d825c274dcfae
Vladimir Shubarin <vhundef@gmail.com>
2017-09-25 23:55:51 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+echo "--------Installing ftp------"
 sudo apt-get install ftp
+echo "+++++++++ Ftp installed +++++++++++"
 HOST='files.000webhost.com'
 USER='vhundef'
 PASSWD='Vhn323884489'
+echo "Trying to open apk"
 FILE='/home/circleci/code/TeamCode/build/outputs/apk/TeamCode-debug.apk'
 
 ftp -n $HOST <<END_SCRIPT
