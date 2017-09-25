9201e9416d58289150a64a497b3cf9f8fc854403
Vladimir Shubarin <vhundef@gmail.com>
2017-09-25 23:29:06 +0300

    Create deploy.sh

---
+++ .circleci/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+HOST='files.000webhost.com'
+USER='vhundef'
+PASSWD='Vhn323884489'
+FILE='/home/circleci/code/TeamCode/build/outputs/apk/TeamCode-debug.apk'
+
+ftp -n $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+put $FILE
+quit
+END_SCRIPT
+exit 0
