e0d73717447e56364c417bf88aae3083baa56333
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 00:29:36 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,3 @@
-#!/bin/sh
-echo "--------Installing ftp------"
-sudo apt-get install ftp
-sudo apt-get install sshpass
-echo "+++++++++ Ftp installed +++++++++++"
-sudo sshpass -p 'Vhn323884489' TeamCode/build/outputs/apk/TeamCode-debug.apk vhundef@files.000webhost.com:/public_html/lastest
-quit
-END_SCRIPT
-exit 0
 #!/bin/bash
 HOST='files.000webhost.com'
 USER='vhundef'
