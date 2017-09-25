6ec419081f54e21e8f0973c3c42e344d6267ccc3
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 00:20:05 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 echo "--------Installing ftp------"
 sudo apt-get install ftp
+sudo apt-get install sshpass
 echo "+++++++++ Ftp installed +++++++++++"
-HOST='files.000webhost.com'
-USER='vhundef'
-PASSWD='Vhn323884489'
-echo "Trying to open apk"
-FILE='TeamCode/build/outputs/apk/TeamCode-debug.apk'
-echo "$FILE"
-ftp -inv $HOST <<EOF
-user $USER $PASSWD
-prompt
-cd /public_html/lastest
-put $FILE
+sshpass -p 'Vhn323884489' TeamCode/build/outputs/apk/TeamCode-debug.apk vhundef@files.000webhost.com:/public_html/lastest
 quit
 END_SCRIPT
 exit 0
