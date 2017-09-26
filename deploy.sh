aaed0db0353066a8a38b053f81c6925a7a0e528a
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 21:49:32 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+sudo apt-get update && sudo apt-get install google-cloud-sdk
 echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
-sudo /opt/google-cloud-sdk/bin/gcloud gsutil cp TeamCode/build/outputs/apk gs://vh-ftc/
+sudo gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+sudo gcloud config set project $GCLOUD_PROJECT
+sudo gcloud gsutil cp TeamCode/build/outputs/apk gs://vh-ftc/
