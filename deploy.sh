e286cccc4e5bb968d39b059b44611cc7acb7616e
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 21:30:29 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-echo "--------Installing ftp------"
-sudo apt-get install ftp
-echo "+++++++++ Ftp installed +++++++++++"
-FILEtoPut='TeamCode-debug.apk'
-
-ftp -n files.000webhost.com <<END_SCRIPT
-quote USER vhundef
-quote PASS Vhn323884489
-lcd TeamCode/build/outputs/apk
-cd /public_html/lastest
-put $FILEtoPut
-
-quit
-END_SCRIPT
-exit 0
+sudo /opt/google-cloud-sdk/bin/gcloud compute copy-files TeamCode/build/outputs/apk/TeamCode-debug.apk vh-ktm:/
