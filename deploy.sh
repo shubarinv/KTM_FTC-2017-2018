c7f67e8a474ec3803eff025aa9845920c3503fdb
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 11:56:17 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 echo "--------Installing ftp------"
 sudo apt-get install ftp
+apt-get install putty
 echo "+++++++++ Ftp installed +++++++++++"
 HOST='files.000webhost.com'
 USER='vhundef'
 PASSWD='Vhn323884489'
 echo "Trying to open apk"
+psftp $HOST -l $USER -pw $PASSWD -v
 
-ftp -inv $HOST <<EOF
-passive
-user $USER $PASSWD
 
-prompt
 
 lcd TeamCode/build/outputs/apk
 cd /public_html/lastest
