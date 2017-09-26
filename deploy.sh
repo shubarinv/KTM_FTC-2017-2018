f310a5ec9fe451ea71285ef153d0c57df90ae98e
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 22:34:58 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 gcloud auth activate-service-account --key-file=vh-gcloud.json
-sudo gcloud config set project 461060431129
+sudo gcloud config set project vh-ktm
 sudo gsutil cp TeamCode/build/outputs/apk/TeamCode-debug.apk gs://vh-ftc/
