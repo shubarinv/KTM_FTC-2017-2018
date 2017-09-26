cc14dfaa3444cf68ed83e78ddc867d11e511fd8a
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 21:45:29 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo /opt/google-cloud-sdk/bin/gcloud compute copy-files TeamCode/build/outputs/apk/TeamCode-debug.apk vh-ktm:/
+echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
+sudo /opt/google-cloud-sdk/bin/gcloud gsutil cp TeamCode/build/outputs/apk gs://vh-ftc/
