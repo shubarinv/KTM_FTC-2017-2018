a329d307c9b4e4f343e3351d47d2057be3718a8c
Vladimir Shubarin <vhundef@gmail.com>
2017-09-26 17:38:32 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ HOST='files.000webhost.com'
 USER='vhundef'
 PASSWD='Vhn323884489'
 echo "Trying to open apk"
-psftp files.000webhost.com -l vhundef -pw Vhn323884489
+psftp $HOST -l vhundef -pw Vhn323884489
 lcd TeamCode/build/outputs/apk
 cd /public_html/lastest
 put TeamCode-debug.apk
