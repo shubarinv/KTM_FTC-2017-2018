7246c0d9a7b7369d1f0b9a52264db0dbc1a70b9a
Vladimir Shubarin <vhundef@gmail.com>
2017-09-27 18:45:10 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ gcloud auth activate-service-account --key-file=vh-gcloud.json
 gcloud config set pass_credentials_to_gsutil true
 sudo gcloud config set project vh-ktm
 yes Y | sudo gcloud config set account vh-424@vh-ktm.iam.gserviceaccount.com
-sudo gsutil config -w
+gcloud auth login
 sudo gsutil cp TeamCode/build/outputs/apk/TeamCode-debug.apk gs://vh-ftc/
