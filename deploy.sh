faeac76cf945ed5e0a17c3df4b4f97a37077184c
Vladimir Shubarin <vhundef@gmail.com>
2017-09-27 20:28:08 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,56 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get install google-cloud-sdk
-gcloud auth activate-service-account --key-file=vh-gcloud.json
-gcloud config set pass_credentials_to_gsutil true
-sudo gcloud config set project vh-ktm
-yes Y | sudo gcloud config set account vh-424@vh-ktm.iam.gserviceaccount.com
-sudo gsutil cp TeamCode/build/outputs/apk/TeamCode-debug.apk gs://vh-ftc/
+#!/bin/sh
+# ideas used from https://gist.github.com/motemen/8595451
+
+# abort the script if there is a non-zero error
+set -e
+
+# show where we are on the machine
+pwd
+
+git remote set-url https://github.com/vhundef/vh_ftc_artifacts.git
+remote=$(git config remote.origin.url)
+
+siteSource="$1"
+
+if [ ! -d "$siteSource" ]
+then
+    echo "Usage: $0 <site source dir>"
+    exit 1
+fi
+
+# make a directory to put the gp-pages branch
+mkdir master
+cd master
+# now lets setup a new repo so we can update the gh-pages branch
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git init
+git remote add --fetch origin "$remote"
+
+# switch into the the gh-pages branch
+if git rev-parse --verify origin/master > /dev/null 2>&1
+then
+    git checkout master
+    # delete any old site as we are going to replace it
+    # Note: this explodes if there aren't any, so moving it here for now
+    git rm -rf .
+else
+    git checkout --orphan master
+fi
+
+# copy over or recompile the new site
+cp -a "../${siteSource}/." .
+
+# stage any changes and new files
+git add -A
+# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+# and push, but send any output to /dev/null to hide anything sensitive
+git push --force --quiet origin master > /dev/null 2>&1
+
+# go back to where we started and remove the gh-pages git repo we made and used
+# for deployment
+cd ..
+rm -rf master
+
+echo "Finished Deployment!"
