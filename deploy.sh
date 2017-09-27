82b3f120c4484ae5041668f3071f4961eb814011
Vladimir Shubarin <vhundef@gmail.com>
2017-09-27 20:53:54 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 echo "--------Installing ftp------"
 sudo apt-get install ftp
 echo "+++++++++ Ftp installed +++++++++++"
-$HOST='files.000webhost.com'
-$USER='vhundef'
-$PASSWD='Vhn323884489'
-$FILEtoPut='TeamCode-debug.apk'
+HOST='files.000webhost.com'
+USER='vhundef'
+PASSWD='Vhn323884489'
+FILEtoPut='TeamCode-debug.apk'
 
 ftp -n $HOST <<END_SCRIPT
 quote USER $USER
