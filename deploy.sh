fb1be8dada9427047921f9c6a69d38cf2aa19041
Vladimir Shubarin <vhundef@gmail.com>
2017-09-27 18:36:56 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 gcloud auth activate-service-account --key-file=vh-gcloud.json
+gcloud config set pass_credentials_to_gsutil true
 sudo gcloud config set project vh-ktm
 yes Y | sudo gcloud config set account vh-424@vh-ktm.iam.gserviceaccount.com
 sudo gsutil config -e
