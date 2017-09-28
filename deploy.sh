89496d1322fbcf5af7c30ba6df63acced258171b
Vladimir Shubarin <vhundef@gmail.com>
2017-09-28 19:44:20 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ IP='vhundef.net'
 USER='admin_vh'
 PASS='Vhn323884489'
 DESTINATION='public_html'
-ftp -p -d -inv $IP<< !EOF
+ftp -d -inv $IP<< !EOF
 user $USER $PASS
 lcd TeamCode/build/outputs/apk
 cd $DESTINATION
