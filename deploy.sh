aed121bca2733cd2722ad6cb3ca81f05daf85377
Vladimir Shubarin <vhundef@gmail.com>
2017-10-10 23:16:07 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ FILE='TeamCode-debug.apk'
 IP='vhundef.net'
 USER='admin_vh'
 PASS='Vhn323884489'
-FName=$1 $FILE
 DESTINATION='public_html'
 ftp -p -d -inv $IP<< !EOF
 user $USER $PASS
@@ -15,6 +14,6 @@ lcd TeamCode/build/outputs/apk
 cd $DESTINATION
 asc
 mput $FILE
-rename $FILE $FName
+rename $FILE $1$FILE
 quit
 !EOF
