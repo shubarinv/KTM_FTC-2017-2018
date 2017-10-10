35266c35a6f728d05854b513c3fbd57dea20de69
Vladimir Shubarin <vhundef@gmail.com>
2017-10-10 23:11:06 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ FILE='TeamCode-debug.apk'
 IP='vhundef.net'
 USER='admin_vh'
 PASS='Vhn323884489'
-FName=$1'-'$File
+FName=$1$FILE
 DESTINATION='public_html'
 ftp -p -d -inv $IP<< !EOF
 user $USER $PASS
