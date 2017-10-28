e6ee63618305482a2099a8b165048962f035ea59
Vladimir Shubarin <vhundef@gmail.com>
2017-10-28 13:31:58 +0300

    AutoOp Related changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Op_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Op_Linear.java
@@ -256,8 +256,8 @@ public class Auto_Op_Linear extends LinearOpMode {
                     } else if (-100 < tX && tX < 100) {
                         if (!wasExecuted) {
                             grab_box(true,false,false,true);
-                            lift_claw(0.1, 100);
-                            set_Motors_Power_timed(-0.2, 0.2, 0.2, -0.2, 1000);//движение вперед
+                            lift_claw(0.2, 250);
+                            set_Motors_Power_timed(-0.2, 0.2, 0.2, -0.2, 4000);//движение вперед
                             set_Motors_Power_timed(-0.2, -0.2, -0.2, -0.2, 1250);//поворот по часовой
                             lift_claw(-0.1, 100);
                             grab_box(false, true, false, false);
