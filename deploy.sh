126f683572fe7dcb7daafb055291c018b6ff1e35
Vladimir Shubarin <vhundef@gmail.com>
2017-12-25 22:20:59 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-echo $2 '\n' $3 >Build.info
-echo "--------Installing ftp------"
-sudo apt-get install ftp
-echo "+++++++++ Ftp installed +++++++++++"
-echo "VARS "$1 $2 $3 $4 $5
 FILE='TeamCode-debug.apk'
 IP='vhundef.net'
-USER=$4
-PASS=$5
+USER=$1
+PASS=$2
 DESTINATION='public_html'
 ftp -p -d -inv $IP<< !EOF
 user $USER $PASS
 cd $DESTINATION
 asc
-mput Build.info
-asc
-mput CHANGELOG.md
 lcd TeamCode/build/outputs/apk
 mput $FILE
-rename $FILE $1-$FILE
 quit
 !EOF
