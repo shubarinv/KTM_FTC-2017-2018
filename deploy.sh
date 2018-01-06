834dc7d4e2d3ac77f53064f270677e888eb41355
Vladimir Shubarin <vhundef@gmail.com>
2018-01-07 00:01:33 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt-get install ftp
+echo $4 '\n'"$5">Build.info
+
+FILE='TeamCode-debug.apk'
+IP='vhundef.net'
+USER=$1
+PASS=$2
+DESTINATION='public_html'
+ftp -p -d -inv $IP<< !EOF
+user $USER $PASS
+cd $DESTINATION
+asc
+mput Build.info
+rename Build.info $3-Build.info
+asc
+lcd TeamCode/build/outputs/apk
+mput $FILE
+rename $FILE $3-$FILE
+quit
+!EOF
