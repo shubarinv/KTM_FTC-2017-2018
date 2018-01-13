54751ee313cfa2cafd5a71671eebdf5b3e2c1630
Vladimir Shubarin <vhundef@gmail.com>
2018-01-13 18:15:25 +0300

    OpMode_2 -->shovel support added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Linear_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Linear_2.java
@@ -40,6 +40,7 @@ public class OpMode_Linear_2 extends LinearOpMode {
   private CRServo s2_bottom_Claw = null;
   private Servo s3_rotation = null;
   private Servo s4_kicker = null;
+  private Servo s5_shovel = null;
   private static final int LED_CHANNEL = 5;
   ColorSensor sensorRGB;
   DeviceInterfaceModule cdim;
@@ -82,16 +83,12 @@ public class OpMode_Linear_2 extends LinearOpMode {
     m5_Lift.setPower(lift_power);
   }
 
-  //rotate claw
-  void rotate_claw(double claw_rotation) { //if rotate true then rotate to  180 . else to 0
-    if (claw_rotation==0) {
-      s3_rotation.setPosition(0.1);
-    } else if(claw_rotation==-1){
-      s3_rotation.setPosition(0);
+  void shovel_trigger(boolean shovel_up, boolean shovel_down){
+    if (shovel_up){
+    s5_shovel.setPosition(1);
     }
-    if(claw_rotation>0){
-      double pos=0.1+claw_rotation/1.1;
-      s3_rotation.setPosition(pos);
+    else if(shovel_down){
+      s5_shovel.setPosition(0);
     }
   }
 
@@ -138,6 +135,8 @@ public class OpMode_Linear_2 extends LinearOpMode {
     s2_bottom_Claw = hardwareMap.get(CRServo.class, "s2 bottom claw");
     s3_rotation = hardwareMap.get(Servo.class, "s3 rotation");
     s4_kicker = hardwareMap.get(Servo.class, "s4 kick");
+    s5_shovel = hardwareMap.get(Servo.class, "s5 shovel");
+
     //-------
     // Most robots need the motor on one side to be reversed to drive forward
     // Reverse the motor that runs backwards when connected directly to the battery
@@ -176,6 +175,8 @@ public class OpMode_Linear_2 extends LinearOpMode {
       float claw_clamp_bottom = gamepad2.right_trigger;
       boolean claw_release_top = gamepad2.left_bumper;
       boolean claw_release_bottom = gamepad2.right_bumper;
+      boolean shovel_down=gamepad2.dpad_down;
+      boolean shovel_up=gamepad2.dpad_up;
       cdim = hardwareMap.deviceInterfaceModule.get("dim");
       //Slide Related
       double slide;
