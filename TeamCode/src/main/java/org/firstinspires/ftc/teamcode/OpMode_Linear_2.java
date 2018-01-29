54d362f5abbc6bfe44380169bd47fc00654a1c72
Vladimir Shubarin <vhundef@gmail.com>
2018-01-29 17:51:33 +0300

    Changed controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Linear_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Linear_2.java
@@ -247,12 +247,13 @@ public class OpMode_Linear_2 extends LinearOpMode {
             if (relic == 0) {
                 if (relic_arm_extend) {
                     setPower_Timed(m6_Relic, 0.2, 500);
-                    s7_relic_arm.setPosition(0.9);
                     if (!touchSensor.isPressed()) {
                         m6_Relic.setPower(0.1);
                     } else {
                         m6_Relic.setPower(0);
+                        s7_relic_arm.setPosition(0.9);
                     }
+
                 }
                 if (relic_arm_halt) {
                     s7_relic_arm.setPosition(0.3);
