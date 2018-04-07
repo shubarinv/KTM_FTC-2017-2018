0c5d976dc0a842eb981f04954969741d1f54c52f
Vladimir Shubarin <vhundef@gmail.com>
2018-04-07 23:44:19 +0300

    Simplified autoRedLeft Added robot.java for common functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOPs/autoRedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOPs/autoRedLeft.java
@@ -29,17 +29,8 @@
 
 package org.firstinspires.ftc.teamcode.AutoOPs;
 
-import android.graphics.Color;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -47,25 +38,14 @@ import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.robot;
 
 import java.util.Objects;
 
 
 @Autonomous(name = "AUTO Red Left", group = "AutoOP")
 //@Disabled
-public class autoRedLeft extends LinearOpMode {
-    /* ADAFRUIT */
-    // we assume that the LED pin of the RGB sensor is connected to
-    // digital port 5 (zero indexed).
-    private static final int LED_CHANNEL = 5;
-    ColorSensor sensorRGB;
-    DeviceInterfaceModule cdim;
-    // hsvValues is an array that will hold the hue, saturation, and value information.
-    float hsvValues[] = {0F, 0F, 0F};
-    // bLedOn represents the state of the LED.
-    boolean bLedOn = false;
-    OpticalDistanceSensor odsSensor;  // Hardware Device Object
-    private CRServo s1RelicExtRet = null;
+public class autoRedLeft extends robot {
     /**
      * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
      * localization engine.
@@ -74,190 +54,55 @@ public class autoRedLeft extends LinearOpMode {
     private boolean wasExecuted = false;
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor m1Drive = null;
-    private DcMotor m2Drive = null;
-    private DcMotor m3Drive = null;
-    private DcMotor m4Drive = null;
-    private DcMotor m5Lift = null;
-    private CRServo s1TopClaw = null;
-    private Servo s4Kicker = null;
-    private Servo s3Rotation = null;
-    private Servo s5Shovel = null;
-    private DcMotor m6Intake = null;
     private boolean isPositioned = false;
-    /*
-     * Functions
-     */
-
-    void putBox() {
-        setMotorsPowerTimed(-0.2, 0.2, 0.2, -0.2, 1200);//движение назад
-        sleep(100);
-        setMotorsPowerTimed(0.2, -0.2, -0.2, 0.2, 300);//движение вперёд
-        rotateClaw(0);
-        setMotorsPowerTimed(0.2, -0.2, -0.2, 0.2, 300);//движение вперёд
-        setMotorsPowerTimed(-0.2, 0.2, 0.2, -0.2, 600);//движение назад
-        setMotorsPowerTimed(0.1, -0.1, -0.1, 0.1, 500);//движение вперёд
-        rotateClaw(0.8);
-    }
-
-    // Rotate claw
-    void rotateClaw(double rotate) { //if rotate true then rotate to  180 . else to 0
-        s3Rotation.setPosition(rotate);
-    }
-
-    void setMotorsPower(double D1_power, double D2_power, double D3_power, double D4_power) { //Warning: Эта функция включит моторы но, выключить их надо будет после выполнения какого либо условия
-        // Send power to wheels
-        m1Drive.setPower(D1_power);
-        m2Drive.setPower(D2_power);
-        m3Drive.setPower(D3_power);
-        m4Drive.setPower(D4_power);
-    }
-
-    void setMotorsPowerTimed(double m1_power, double m2_power, double m3_power, double m4_power, long ms) {
-        m1Drive.setPower(m1_power);
-        m2Drive.setPower(m2_power);
-        m3Drive.setPower(m3_power);
-        m4Drive.setPower(m4_power);
-        sleep(ms);
-        chassisStopMovement();
-    }
-
-    void chassisStopMovement() {
-        m1Drive.setPower(0);
-        m2Drive.setPower(0);
-        m3Drive.setPower(0);
-        m4Drive.setPower(0);
-    }
-
-    String getColor() {
-        // button is transitioning to a pressed state. Toggle the LED.
-        cdim.setDigitalChannelState(LED_CHANNEL, true);
-
-        double[] hue_arr = new double[5];
-        //для точности 4 измерения
-        for (int j = 0; j < 4; j++) {
-            // convert the RGB values to HSV values.
-            telemetry.addData("Blue", sensorRGB.blue());
-            telemetry.addData("Red", sensorRGB.red());
-            telemetry.update();
-            sleep(500);
-            Color.RGBToHSV((sensorRGB.red() * 255) / 800, (sensorRGB.green() * 255) / 800, (sensorRGB.blue() * 255) / 800, hsvValues);
-            double hue = hsvValues[0];
-            hue_arr[j] = hue;
-        }
-
-        //Находим среднее арифметическое
-
-        double hue_sr = 0;
-        for (int j = 0; j < 4; j++) {
-            hue_sr += hue_arr[j];
-        }
-        hue_sr = hue_sr / 4;
-        //
-        if (hue_sr > 110 && hue_sr < 290) {
-            return "Blue";
-        } else {
-            return "Red";
-        }
-    }
-
 
     @Override
     public void runOpMode() {
-        s1RelicExtRet = hardwareMap.get(CRServo.class, "s1 top claw");
-        s1RelicExtRet.setPower(0);
-        /*
-         * To start up Vuforia, tell it the view that we wish to use for camera monitor (on the RC phone);
-         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
-         */
+        //Vuforia Related Variables
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        //Vuforia API key
         parameters.vuforiaLicenseKey = "AfQcHkL/////AAAAGd5Auzk+t0CxnAw8xKONnjke+r6gFs0KfKK8LsB35FsX6bnhXZmEN+0f3blTVk7nI4xjKNob63Ps1Jpp/JS25hHc083okOZzcTsBlA5qz2hJK3LFNWyZv59kjCUyqbc3qS7dTXJ4i4/JD9t+IeyvGH9G9xPwV7DNmcuNeT7o+YDn3cI7zgUcVcrdFM8t22/wGkmiCz5TfY5A0BMETyriYX6BzlVuwGtMfXdp9CYDQ+ZhZTRNjPfvKlNyLLxVycIiM1p4nprW2UnySO11fmTkUZR9Ofqr+gbHj0VNm7gUEz77s/cHTl+swX84pxpOhm1QJeO0wuNw4c5siQpizcWHPMhJCDRFqRmTQ3LBpcMJWjTx";
-        /*
-         * We also indicate which camera on the RC that we wish to use.
-         * Here we chose the back (HiRes) camera (for greater range), but
-         * for a competition robot, the front camera might be more convenient.
-         */
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-
         VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
         VuforiaTrackable relicTemplate = relicTrackables.get(0);
         relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
-        /*
-         * Initialize the drive system variables.
-         * The init() method of the hardware class does all the work here
-         */
 
-        //Обработка исключений
-        // m1_drive
+        //Инициализация наследуется из robot.java
+        initHW(hardwareMap);
 
-        try {
-            //Chassis
-            m1Drive = hardwareMap.get(DcMotor.class, "m1 drive");
-            m2Drive = hardwareMap.get(DcMotor.class, "m2 drive");
-            m3Drive = hardwareMap.get(DcMotor.class, "m3 drive");
-            m4Drive = hardwareMap.get(DcMotor.class, "m4 drive");
-
-            m5Lift = hardwareMap.get(DcMotor.class, "m5 lift");
-            s1TopClaw = hardwareMap.get(CRServo.class, "s1 top claw");
-            s4Kicker = hardwareMap.get(Servo.class, "s4 kick");
-            odsSensor = hardwareMap.get(OpticalDistanceSensor.class, "sensor_ods");
-            s3Rotation = hardwareMap.get(Servo.class, "s3 rotation");
-            s5Shovel = hardwareMap.get(Servo.class, "s5 shovel");
-            m6Intake = hardwareMap.get(DcMotor.class, "m6 intake");
-        } catch (RuntimeException e) {
-            telemetry.addData("INIT", "Error occurred during init");
-            telemetry.update();
-        }
-        // Конец обработки исключений
-        m1Drive.setDirection(DcMotor.Direction.FORWARD);
-        m2Drive.setDirection(DcMotor.Direction.FORWARD);
-        m3Drive.setDirection(DcMotor.Direction.FORWARD);
-        m4Drive.setDirection(DcMotor.Direction.FORWARD);
-        /* AdaFruit */
-
-        // get a reference to our DeviceInterfaceModule object.
-        cdim = hardwareMap.deviceInterfaceModule.get("dim");
-
-        // set the digital channel to output mode.
-        // remember, the Adafruit sensor is actually two devices.
-        // It's an I2C sensor and it's also an LED that can be turned on or off.
+        //Это для LED
         cdim.setDigitalChannelMode(LED_CHANNEL, DigitalChannel.Mode.OUTPUT);
 
-        // get a reference to our ColorSensor object.
-        sensorRGB = hardwareMap.colorSensor.get("sensor_color");
 
-        // turn the LED on in the beginning, just so user will know that the sensor is active.
-        cdim.setDigitalChannelState(LED_CHANNEL, bLedOn);
-        telemetry.addData("AdaFruit", "Ready");
-        telemetry.update();
+        cdim.setDigitalChannelState(LED_CHANNEL, false); // Отключаем LED
+        log("Ready");
+        runtime.reset();
         waitForStart();
 
         relicTrackables.activate();
         while (opModeIsActive()) {
-            s1RelicExtRet.setPower(0);
             if (wasExecuted) {
                 telemetry.addData("Autonomous: ", "DONE");
             }
 
             if (!wasExecuted) {
+                /* Это определение VuMark */
+                log("Смотрим Vumark", runtime.seconds());
                 RelicRecoveryVuMark vuMark = null;
-                for (int tick = 5; tick < 4000; tick += 1) {
+                for (int tick = 0; tick < 4000; tick++) {
                     vuMark = RelicRecoveryVuMark.from(relicTemplate);
                     if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
                         break;
                     }
-
                 }
 
                 telemetry.addData("VUMARK", vuMark);
                 telemetry.update();
-        /*
-        STEP 1 -Trying to kick jewel
-        */
 
+                log("VuMark " + String.valueOf(vuMark));
+
+                /*  Это для сбития Jewel */
                 rotateClaw(0.8);// so that boxes won't fall off
                 sleep(800);
                 s4Kicker.setPosition(0.75);
@@ -265,6 +110,7 @@ public class autoRedLeft extends LinearOpMode {
 
                 telemetry.addData("Step-1", "Running");
                 String jewel_color = getColor();
+                log("Jewel color: " + jewel_color + "Runtime ", runtime.seconds());
                 telemetry.addData("AdaFruit", jewel_color);
                 telemetry.update();
                 if (Objects.equals(jewel_color, "Blue")) {
@@ -278,40 +124,37 @@ public class autoRedLeft extends LinearOpMode {
                 cdim.setDigitalChannelState(LED_CHANNEL, false);
                 // requestOpModeStop();
 
-        /*
-        STEP 2 -Cryptobox related
-        */
-                setMotorsPowerTimed(-0.2, 0.2, 0.2, -0.2, 1000);//движение вперёд
+                /*  Это съезд с камня и езда с поиском линии до CryptoBox */
+                setMotorsPowerTimed(-0.2, 0.2, 0.2, -0.2, 1000);// Съезд с камня
 
+                /* Поиск Среднего Арифметического FieldColorSR */
                 double fieldColor;
                 int tick;
-                double fieldColorReadings = 0;
+                double fieldColorReadings = 0; // эта переменная нужна для хранения суммы показаний датчика линии
                 for (tick = 0; tick < 600; tick += 10) {
                     setMotorsPower(-0.15, 0.15, -0.15, 0.15);
                     fieldColor = odsSensor.getLightDetected();
                     fieldColorReadings += fieldColor;
-                    //telemetry.addData("Readings ", fieldColor);
-                    //telemetry.update();
                     sleep(10);
                 }
                 setMotorsPower(0, 0, 0, 0);
 
-                double fieldColorSR = fieldColorReadings / 60;
+                double fieldColorSR = fieldColorReadings / 60; //Среднее арифметическое
+                log("Среднее арифметическое", fieldColorSR);
                 telemetry.addData("SR", fieldColorSR);
                 telemetry.update();
 
-                sleep(3000);
+                sleep(2000);
+
+                /* Поиск первой линии */
                 for (tick = 0; tick < 2000; tick += 2) {
-                    //telemetry.addData("Centring loop", "iteration: " + tick);
-                    //telemetry.addData(" ", odsSensor.getLightDetected());
-                    //telemetry.update();
                     fieldColor = odsSensor.getLightDetected();
                     setMotorsPower(-0.15, 0.15, 0.15, -0.15);
                     if (fieldColor > fieldColorSR * 1.5) {
+                        log("Found First Line " + fieldColor, runtime.seconds());
                         telemetry.addData("Centring loop", "line Found 1");
                         //  telemetry.addData("Centring loop", fieldColor);
                         telemetry.update();
-
                         break;
                     }
                     if (isStopRequested()) {
@@ -321,7 +164,8 @@ public class autoRedLeft extends LinearOpMode {
                 }
                 setMotorsPower(0, 0, 0, 0);
                 sleep(1000);
-                sleep(200);
+
+                /* Пытаемся потерять линию */
                 for (tick = 5; tick < 500; tick += 2) {
                     setMotorsPower(-0.15, 0.15, 0.15, -0.15);
                     if (odsSensor.getLightDetected() < fieldColorSR * 1.3) {
@@ -330,11 +174,14 @@ public class autoRedLeft extends LinearOpMode {
                     }
                     sleep(2);
                 }
+                log("Потеряна Первая линия ", runtime.seconds());
                 telemetry.addData("LINE", "1 line LOS");
                 telemetry.update();
                 sleep(1000);
                 chassisStopMovement();
                 int drivetime = 0;
+
+                /* Едем и ищем вторую линию */
                 while (odsSensor.getLightDetected() < fieldColorSR * 1.3) {
                     cdim.setDigitalChannelState(LED_CHANNEL, true);
                     if (isStopRequested()) {
@@ -349,16 +196,23 @@ public class autoRedLeft extends LinearOpMode {
                         break;
                     }
                 }
+                /* Если нашли линию */
                 if (odsSensor.getLightDetected() > fieldColorSR * 1.5) {
+                    log("Вторая линия найдена " + odsSensor.getLightDetected(), runtime.seconds());
                     telemetry.addData("Centring loop", "line Found 2 (break)");
                     telemetry.update();
                     sleep(400);
                     cdim.setDigitalChannelState(LED_CHANNEL, false);
                     isPositioned = true;
+
                     setMotorsPowerTimed(0.2, -0.2, -0.2, 0.2, (drivetime / 4));
+                    log("Вернулся к центральной полке ", runtime.seconds());
                     sleep(500);
                 }
+
+                /* Начало движения к нужной полке */
                 setMotorsPowerTimed(0.2, 0.2, 0.2, 0.2, 800);//поворот против часовой
+                log("Повернулся на 90 градусов ", runtime.seconds());
                 if (vuMark == RelicRecoveryVuMark.RIGHT) {
                     telemetry.addData("Vumark", " RIGHT");
                     telemetry.update();
@@ -371,13 +225,12 @@ public class autoRedLeft extends LinearOpMode {
                     telemetry.addData("Vumark", " LEFT");
                     telemetry.update();
                     setMotorsPowerTimed(-0.1, -0.1, 0.1, 0.1, 300);// Slide right
-                } else {
-                    telemetry.addData("Line", "(X)NOT VISIBLE");
-                    telemetry.update();
-
                 }
                 putBox();
                 wasExecuted = true;
+                telemetry.clearAll();
+                telemetry.addData("LOG", printLog());
+                telemetry.update();
             }
         }
     }
